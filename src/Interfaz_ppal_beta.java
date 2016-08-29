f8379b900cb80494f59773b95c8df62d65a0cdb8
rdatop <rdatop@yahoo.com.ar>
2016-08-29 04:03:14 -0300

    Es lo que pude hacer todo el finde, verifica funcionamiento seguro hay que refactorizar metodos o dividirlo en clases, para eso vos tenes mas criterio que yo lejos, el sabado amaneci con esta matriz me revento

---
+++ src/Interfaz_ppal_beta.java
@@ -0,0 +1,243 @@
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
+import Juego.Matriz;
+
+
+public class Interfaz_ppal_beta {
+
+	private static final JLabel[][] matriz = new JLabel[4][4];
+	private JFrame frmMatriz;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaz_ppal_beta window = new Interfaz_ppal_beta();
+					window.frmMatriz.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interfaz_ppal_beta() {
+		initialize(new Matriz());
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize(final Matriz juego) {
+		frmMatriz = new JFrame();
+		
+		// Control de teclado lo saque de lo de la profe
+		frmMatriz.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_UP:
+						frmMatriz.setTitle("Arriba");
+						juego.MoverHaciaArriba();
+						juego.AsignaPosRandom();
+						dibujar(juego);
+						break;
+					case KeyEvent.VK_DOWN:
+						frmMatriz.setTitle("Abajo");
+						juego.MoverHaciaAbajo();
+						juego.AsignaPosRandom();
+						dibujar(juego);
+						break;
+					case KeyEvent.VK_LEFT:
+						frmMatriz.setTitle("Izq");
+						juego.MoverHaciaIzq();	
+						juego.AsignaPosRandom();
+						dibujar(juego);
+						break;
+					case KeyEvent.VK_RIGHT:
+						frmMatriz.setTitle("Der");
+						juego.MoverHaciaDer();	
+						juego.AsignaPosRandom();
+						dibujar(juego);
+						break;
+					case KeyEvent.VK_ESCAPE:
+						frmMatriz.setTitle("salir");
+						break;
+				}
+			}
+		});
+		
+		frmMatriz.setResizable(false);
+		frmMatriz.setTitle("Matriz");
+		frmMatriz.setBounds(100, 100, 480, 480);
+		frmMatriz.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Se crea una matriz 4x4 para acomodar las etiquetas
+		frmMatriz.getContentPane().setLayout(new GridLayout(4, 4));
+
+		
+		juego.IniciarMatriz();
+		juego.AsignaPosRandom();
+		juego.AsignaPosRandom();
+
+		Border borde = BorderFactory.createLineBorder(Color.DARK_GRAY, 2);
+
+		// Ahora se crea una matriz de JLabel
+		// no puedo hacer q se vea desde Dsign porque se despelota
+		// trate de hacer algo para que veas esto hacelo de nuevo vos porque es una porqueria
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				int numero = juego.ObtenerElem(i, j);
+				
+				// Se crean las etiquetas
+				if (numero != 0) {
+					matriz[i][j] = new JLabel(String.valueOf(juego.ObtenerElem(i, j)));
+					matriz[i][j].setBackground(Color.CYAN);//.lightGray
+				} else {
+					matriz[i][j] = new JLabel(String.valueOf(""));
+				}
+				
+				// Para que se pueda ver el color de fondo
+				matriz[i][j].setOpaque(true);
+				// centrado de la etiqueta
+				matriz[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+				// borde de etiqueta
+				matriz[i][j].setBorder(borde);
+				// se colocan etiquetas en formulario
+				frmMatriz.getContentPane().add(matriz[i][j]);
+			}
+		}
+
+		
+		// Se crean los textos que representan la matriz
+		JLabel lbl1 = new JLabel("i0-j0");
+		JLabel lbl2 = new JLabel("i0-j1");
+		JLabel lbl3 = new JLabel("i0-j2");
+		JLabel lbl4 = new JLabel("i0-j3");
+		JLabel lbl5 = new JLabel("i1-j0");
+		JLabel lbl6 = new JLabel("i1-j1");
+		JLabel lbl7 = new JLabel("i1-j2");
+		JLabel lbl8 = new JLabel("i1-j3");
+		JLabel lbl9 = new JLabel("i2-j0");
+		JLabel lbl10 = new JLabel("i2-j1");
+		JLabel lbl11 = new JLabel("i2-j2");
+		JLabel lbl12 = new JLabel("i2-j3");
+		JLabel lbl13 = new JLabel("i3-j0");
+		JLabel lbl14 = new JLabel("i3-j1");
+		JLabel lbl15 = new JLabel("i3-j2");
+		JLabel lbl16 = new JLabel("i3-j3");
+
+		// centrado de etiquetas
+		lbl1.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl2.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl3.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl4.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl5.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl6.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl7.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl8.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl9.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl10.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl11.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl12.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl13.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl14.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl15.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl16.setHorizontalAlignment(SwingConstants.CENTER);
+
+		// borde de etiquetas
+		lbl1.setBorder(borde);
+		lbl2.setBorder(borde);
+		lbl3.setBorder(borde);
+		lbl4.setBorder(borde);
+		lbl5.setBorder(borde);
+		lbl6.setBorder(borde);
+		lbl7.setBorder(borde);
+		lbl8.setBorder(borde);
+		lbl9.setBorder(borde);
+		lbl10.setBorder(borde);
+		lbl11.setBorder(borde);
+		lbl12.setBorder(borde);
+		lbl13.setBorder(borde);
+		lbl14.setBorder(borde);
+		lbl15.setBorder(borde);
+		lbl16.setBorder(borde);
+
+		// Se agregan las etiquetas al formulario lo comente porque si no se despelota
+		/*
+		frmMatriz.getContentPane().add(lbl1);
+		frmMatriz.getContentPane().add(lbl2);
+		frmMatriz.getContentPane().add(lbl3);
+		frmMatriz.getContentPane().add(lbl4);
+		frmMatriz.getContentPane().add(lbl5);
+		frmMatriz.getContentPane().add(lbl6);
+		frmMatriz.getContentPane().add(lbl7);
+		frmMatriz.getContentPane().add(lbl8);
+		frmMatriz.getContentPane().add(lbl9);
+		frmMatriz.getContentPane().add(lbl10);
+		frmMatriz.getContentPane().add(lbl11);
+		frmMatriz.getContentPane().add(lbl12);
+		frmMatriz.getContentPane().add(lbl13);
+		frmMatriz.getContentPane().add(lbl14);
+		frmMatriz.getContentPane().add(lbl15);
+		frmMatriz.getContentPane().add(lbl16);
+		*/
+	}
+
+	private void dibujar(Matriz juego) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				int numero = juego.ObtenerElem(i, j);
+				
+				switch (numero) {
+					case 0:
+						matriz[i][j].setText("");
+						matriz[i][j].setBackground(Color.LIGHT_GRAY);						
+						break;
+					case 2:
+						matriz[i][j].setText(String.valueOf(numero));
+						matriz[i][j].setBackground(Color.CYAN);
+						break;
+					case 4:
+						matriz[i][j].setText(String.valueOf(numero));
+						matriz[i][j].setBackground(Color.YELLOW);
+						break;
+					case 8:
+						matriz[i][j].setText(String.valueOf(numero));
+						matriz[i][j].setBackground(Color.ORANGE);
+						break;
+					case 16:
+						matriz[i][j].setText(String.valueOf(numero));
+						matriz[i][j].setBackground(Color.pink);
+						break;
+					case 32:
+						matriz[i][j].setText(String.valueOf(numero));
+						matriz[i][j].setBackground(Color.red);
+						break;
+					default:
+						matriz[i][j].setText(String.valueOf(numero));
+						matriz[i][j].setBackground(Color.DARK_GRAY);						
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ src/Juego/Matriz.java
@@ -0,0 +1,252 @@
+package Juego;
+
+import java.util.Random;
+
+public class Matriz {
+	
+	//variable de instancia
+	private int[][] _MatrizActual;
+		
+	// Constructor
+	public Matriz() {
+		_MatrizActual = new int[4][4];
+	}
+	
+	// Inicializa toda la matriz en cero
+	public void IniciarMatriz() {
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {//por cada filas
+			for (int columna = 0; columna < _MatrizActual.length; columna++) {//todas las columnas
+				PisarElem(fila, columna, 0);//null
+			}
+		}
+	}
+
+	/**-------Generacion aleatoria de Nro(s) y Posiciones------*/
+	// Numero aleatorio entre 2 y 4 (valores a sumar)
+	public int NroRandom() {
+		int nro=2;
+		Random rnd=new Random();
+		return (int) Math.pow(nro,rnd.nextInt(2)+1);
+	}
+	
+	// Numero aleatorio entre 0 y 3 (coordenadas de la matriz)
+	public int PosicionesRandom() {
+		int posiciones=4;
+		Random rnd=new Random();
+		return (int) rnd.nextInt(posiciones);//Math.floor(Math.random() * 4);
+	}
+	
+	// Asigna posiciones aleatorias para llenado (coordenadas)
+	public void AsignaPosRandom() {
+		int fila = PosicionesRandom();
+	    int columna = PosicionesRandom();
+	    
+	    while (ObtenerElem(fila, columna)!=0) {
+	      fila = PosicionesRandom();
+	      columna = PosicionesRandom();
+	    }
+	    PisarElem(fila, columna, NroRandom());
+	}
+	/**--------------------------------------------------------*/
+	
+	// Obtener elemento segun coordenada
+	public int ObtenerElem(int fila, int columna) {
+		return _MatrizActual[fila][columna];
+	}
+		
+	// Pisar elemento segun coordenada
+	public void PisarElem(int fila, int columna, int elemento) {
+		this._MatrizActual[fila][columna] = elemento;
+	}
+		
+	// Obtener una fila de la matriz y colocarla en un arreglo (movimiento en bloques)
+	public int[] ObtenerFila(int fila) {
+		int[] filaAux = new int[4];
+		for (int j = 0; j < 4; j++) {
+			filaAux[j] = ObtenerElem(fila, j);
+		}
+		return filaAux;
+	}
+	
+	// Entrega una fila de la matriz con un bloque de elementos para ser desplazados
+	public int[] ObtenerFilaMovible(int[] fila){
+		int ElemMovibles = 0;
+		for (int i = 0; i < fila.length; i++) {
+			if (fila[i] != 0) {
+				ElemMovibles++;
+			}
+		}
+		int TamañoDelBloque = 0;
+		int[] BloqueMovible = new int[ElemMovibles];
+		for (int i = 0; i < fila.length; i++) {
+			if (fila[i] != 0) {
+				BloqueMovible[TamañoDelBloque] = fila[i];
+				TamañoDelBloque++;
+			}
+		}
+		return BloqueMovible;
+	}
+	
+	/**------Suma y desplaza elementos Izq/Der/Arriba/Abajo--------*/
+	//Entrega un arreglo redimencionado con la suma hacia la Izq
+	public int[] SumaFilaHaciaIzq(int[] fila) {
+		int[] filaRedimencionada = ObtenerFilaMovible(fila);
+		if (filaRedimencionada.length >= 2){//si la fila tiene 2 o mas elementos
+			for (int i = 0; i < filaRedimencionada.length - 1; i++) {//recorro hacia -->
+				if (filaRedimencionada[i] == filaRedimencionada[i + 1]) {
+					filaRedimencionada[i] = filaRedimencionada[i] * 2;
+					filaRedimencionada[i + 1] = 0;//null//i++;
+				}
+			}
+		}
+		filaRedimencionada = ObtenerFilaMovible(filaRedimencionada);//redimenciona si no queda lugar libre
+		return filaRedimencionada;
+	}
+	
+	//Entrega un arreglo redimencionado con la suma hacia la Der
+	public int[] SumaFilaHaciaDer(int[] fila) {
+		int[] filaRedimencionada = ObtenerFilaMovible(fila);
+		if (filaRedimencionada.length >= 2){//si la fila tiene 2 o mas elementos
+			for (int i = filaRedimencionada.length - 1; i > 0; i--) {//recorro hacia <--
+				if (filaRedimencionada[i] == filaRedimencionada[i - 1]) {
+					filaRedimencionada[i] = filaRedimencionada[i] * 2;
+					filaRedimencionada[i - 1] = 0;//null//i--;
+				}
+			}
+		}
+		filaRedimencionada = ObtenerFilaMovible(filaRedimencionada);//redimenciona si no queda lugar libre
+		return filaRedimencionada;
+	}
+	
+	// Apila los elementos desiguales a la Izq
+	public void MoverHaciaIzq() {
+		//_MatrizAnterior = _MatrizActual;
+		int[][] MatrizAux = new int[_MatrizActual.length][_MatrizActual.length];
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {
+			int[] FilaRedimencionada = SumaFilaHaciaIzq(this._MatrizActual[fila]);//valores
+			for (int columna = 0; columna < _MatrizActual.length; columna++) {
+				if (columna < FilaRedimencionada.length) {//si tiene valor
+					MatrizAux[fila][columna] = FilaRedimencionada[columna];//mueve a la izq valores
+				}
+			}
+		}
+		this._MatrizActual = MatrizAux;
+	}
+	
+	// Apila los elementos desiguales a la Izq-----no me gusta no lo puedo hacer de otra manera(refactorizar)
+	public void MoverHaciaDer() {
+		//_MatrizAnterior = _MatrizActual;
+		int[][] MatrizAux = new int[_MatrizActual.length][_MatrizActual.length];
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {
+			int[] FilaRedimencionada = SumaFilaHaciaDer(_MatrizActual[fila]);
+			int columnaAux = 0;
+			for (int columna = 0; columna<_MatrizActual.length; columna++) {
+				if (columna < _MatrizActual.length - FilaRedimencionada.length) {
+					MatrizAux[fila][columna] = 0;
+				} else {
+					MatrizAux[fila][columna] = FilaRedimencionada[columnaAux];//posicion
+					columnaAux++;
+				}
+			}
+		}
+		_MatrizActual = MatrizAux;
+	}
+	
+	// Apila los elementos desiguales hacia abajo moviendo la matriz 90 grados (dos veces) y moviendo a la Derecha
+	public void MoverHaciaAbajo() {
+		//mueve 90 grados la primera vez
+		int[][] MatrizAux = new int[_MatrizActual.length][_MatrizActual.length];
+		_MatrizActual.toString();
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {//recorre filas
+			int[] FilaActual = ObtenerFila(fila);//guarda los elementos no nulos de la fila en un arreglo
+			
+			for (int columna = 0; columna < _MatrizActual.length; columna++) {//por cada fila recorre columnas
+				if (columna < FilaActual.length) {//si la iteracion es < a la cant de elementos de la fila con valores
+					MatrizAux[columna][fila] = FilaActual[columna];//rota 
+				} else {
+					MatrizAux[columna][fila] = 0;
+				}
+			}
+		}
+		MatrizAux.toString();
+		_MatrizActual = MatrizAux;
+		
+		//una vez que la matriz esta bolteada mueve hacia la derecha
+		MoverHaciaDer();
+		
+		//despues la coloca de forma normal
+		int[][] MatrizAux1 = new int[_MatrizActual.length][_MatrizActual.length];
+		_MatrizActual.toString();
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {//recorre filas
+			int[] FilaActual = ObtenerFila(fila);//guarda los elementos no nulos de la fila en un arreglo
+			
+			for (int columna = 0; columna < _MatrizActual.length; columna++) {//por cada fila recorre columnas
+				if (columna < FilaActual.length) {//si la iteracion es < a la cant de elementos de la fila con valores
+					MatrizAux1[columna][fila] = FilaActual[columna];//rota 
+				} else {
+					MatrizAux1[columna][fila] = 0;
+				}
+			}
+		}
+		MatrizAux1.toString();
+		_MatrizActual = MatrizAux1;
+	}
+
+	// Apila los elementos desiguales hacia abajo moviendo la matriz 90 grados (dos veces) y moviendo a la Derecha	
+	public void MoverHaciaArriba() {
+	//mueve 90 grados la primera vez
+		int[][] MatrizAux = new int[_MatrizActual.length][_MatrizActual.length];
+		_MatrizActual.toString();
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {//recorre filas
+			int[] FilaActual = ObtenerFila(fila);//guarda los elementos no nulos de la fila en un arreglo
+			
+			for (int columna = 0; columna < _MatrizActual.length; columna++) {//por cada fila recorre columnas
+				if (columna < FilaActual.length) {//si la iteracion es < a la cant de elementos de la fila con valores
+					MatrizAux[columna][fila] = FilaActual[columna];//rota 
+				} else {
+					MatrizAux[columna][fila] = 0;
+				}
+			}
+		}
+		MatrizAux.toString();
+		_MatrizActual = MatrizAux;
+			
+		//una vez que la matriz esta bolteada mueve hacia la derecha
+		MoverHaciaIzq();
+				
+		//despues la coloca de forma normal
+		int[][] MatrizAux1 = new int[_MatrizActual.length][_MatrizActual.length];
+		_MatrizActual.toString();
+		for (int fila = 0; fila < _MatrizActual.length; fila++) {//recorre filas
+			int[] FilaActual = ObtenerFila(fila);//guarda los elementos no nulos de la fila en un arreglo
+					
+			for (int columna = 0; columna < _MatrizActual.length; columna++) {//por cada fila recorre columnas
+				if (columna < FilaActual.length) {//si la iteracion es < a la cant de elementos de la fila con valores
+					MatrizAux1[columna][fila] = FilaActual[columna];//rota 
+				} else {
+					MatrizAux1[columna][fila] = 0;
+				}
+			}
+		}
+		MatrizAux1.toString();
+		_MatrizActual = MatrizAux1;
+	}
+	/**-------------------------------------------------------------------------------------*/
+	
+	//representacion de la matriz	
+	  @Override
+	  public String toString() {
+		  String s = "";
+		  
+		  for (int i = 0; i < this._MatrizActual.length; i++) {
+			  for (int j = 0; j < this._MatrizActual.length; j++) {
+				  s += this._MatrizActual[i][j];
+				  s += "\t";
+			  }
+			  
+			  s += "\n";
+		  }
+		  
+		  return s;
+	  }
+}
