199d4db3694e9f3ebd197d9b553dd18d6d610bed
Pablo <pablosrecagno88@gmail.com>
2016-09-13 03:56:21 -0300

    VentanaPuntajes muestra nombres de usuario y puntajes pero esta hardcodeada por el momento.

--- src/vistas/VentanaPuntajes.java
+++ src/vistas/VentanaPuntajes.java
@@ -6,50 +6,21 @@ import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.*;
 
+
 @SuppressWarnings("serial")
 public class VentanaPuntajes extends JFrame {
 	public VentanaPuntajes() {
 		
-		super("Ejemplo 2");
-		//array bidimencional de objetos con los datos de la tabla
-		Object[][] data = {
-				{"Mary", "Campione",
-					"Esquiar", new Integer(5), new Boolean(false)},
-				{"Lhucas", "Huml",
-						"Patinar", new Integer(3), new Boolean(true)},
-				{"Kathya", "Walrath",
-							"Escalar", new Integer(2), new Boolean(false)},
-				{"Marcus", "Andrews",
-								"Correr", new Integer(7), new Boolean(true)},
-				{"Angela", "Lalth",
-									"Nadar", new Integer(4), new Boolean(false)}
-		};
-		
+		Object[] newRow={"Juano",3054};
+				
 		//array de String's con los títulos de las columnas
-		String[] columnNames = {"Nombre",
-				"Apellido",
-				"Pasatiempo",
-				"Años de Practica",
-		"Soltero(a)"};
+		String[] columnNames = {"Nombre usuario","Puntaje"};
 		
 		//creamos el Modelo de la tabla con los datos anteriores
-		DefaultTableModel dtm= new DefaultTableModel(data, columnNames);
+		DefaultTableModel dtm= new DefaultTableModel(new Object[0][0] , columnNames);
+		dtm.addRow(newRow);
 		//se crea la Tabla con el modelo DefaultTableModel
 		final JTable table = new JTable(dtm);
-		// una vez creada la tabla con su modelo
-		// podemos agregar columnas
-		String[] newColumn= {"Flan",
-				"Pastel",
-				"Helado",
-				"Barquillo",
-		"Manzana" };
-		dtm.addColumn("Postre",newColumn);
-		//filas
-		Object[] newRow={"Pepe", "Grillo",
-				"Tenis", new Integer(5), new Boolean(false), "Pera"};
-		dtm.addRow(newRow);
-		//o modificar una celda en especifico
-		dtm.setValueAt("Catherine", 1, 1);
 		//se define el tamaño
 		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		//Creamos un JscrollPane y le agregamos la JTable
